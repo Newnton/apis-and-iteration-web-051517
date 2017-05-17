043603a635be92a824d978e3c86f54e363c42615
Zach Dubno <zach@Zach-Dubnos-MacBook-Pro.local>
2017-05-17 15:32:09 -0400

    finished lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,57 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
+def get_film_data(film_ary)
+  film_ary.map do |film_URL|
+    foo = {}
+    film_data = RestClient.get(film_URL)
+    film_data_hash = JSON.parse(film_data)
+    film_data_hash.each do |key, value|
+      if key == "title"
+        foo[key] = value
+      elsif key == "episode_id"
+        foo[key] = value
+      end
+    end
+    foo
+  end
+end
+
+def find_films(character_hash, character)
+  ret = []
+  counter = -1
+  character_hash["results"].each do |person|
+    counter += 1
+    person.each do |key, values|
+      if key == "name" && values.downcase == character.downcase
+        ret = character_hash["results"][counter]["films"]
+      end
+    end
+  end
+  ret
+end
+
+def format_film_data(character_hash, character)
+  get_film_data(find_films(character_hash, character)).sort do |moviea, movieb|
+    moviea["episode_id"] <=> movieb["episode_id"]
+  end
+end
+
 def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  puts format_film_data(character_hash, character)
+  #foo = 1
+  # while foo <= 9
+  #   all_characters = RestClient.get("http://www.swapi.co/api/people/?page=#{foo}")
+  #   character_hash = JSON.parse(all_characters)
+  #   if format_film_data(character_hash, character) == nil
+  #     foo += 1
+  #   else
+  #     foo += 1
+  #     puts format_film_data(character_hash, character)
+  #   end
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,24 @@
 def welcome
-  # puts out a welcome message here!
+  puts "
+                    8888888888  888    88888
+                   88     88   88 88   88  88
+                    8888  88  88   88  88888
+                       88 88 888888888 88   88
+                88888888  88 88     88 88    888888
+
+                88  88  88   888    88888    888888
+                88  88  88  88 88   88  88  88
+                88 8888 88 88   88  88888    8888
+                 888  888 888888888 88   88     88
+                  88  88  88     88 88    8888888
+ "
 end
 
 def get_character_from_user
   puts "please enter a character"
+  gets.chomp
   # use gets to capture the user's input. This method should return that input, downcased.
 end
+#
+# get-ui()
+# iterator()
